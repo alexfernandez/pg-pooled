964a4ebeee3ef2ca9d53e0ebf715cba872635c2f
Alex Fernandez <alex.fernandez@mediasmart.es>
2015-01-04 18:37:28 +0100

    Added index as main entry point.

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Export pooled pg.
+ * (C) 2015 Alex Fernández.
+ */
+
+
+// requires
+var server = require('./lib/server.js');
+
+// exports
+exports.start = server.start;
+
