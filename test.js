297c9af0fc3c66eefef1a3593946568bb0353960
Alex Fernandez <alex.fernandez@mediasmart.es>
2015-01-04 18:39:49 +0100

    Test file.

---
+++ test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Run package tests.
+ * (C) 2013 Alex Fernández.
+ */
+
+// requires
+var testing = require('testing');
+
+
+/**
+ * Run all module tests.
+ */
+exports.test = function(callback)
+{
+	var tests = {};
+	var libs = ['server'];
+	libs.forEach(function(lib)
+	{
+		tests[lib] = require('./lib/' + lib + '.js').test;
+	});
+	testing.run(tests, callback);
+};
+
+// run tests if invoked directly
+if (__filename == process.argv[1])
+{
+	exports.test(testing.show);
+}
+
