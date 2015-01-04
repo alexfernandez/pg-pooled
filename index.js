755fe694494364cb232fb4706f71c61632915e56
Alex Fernandez <alex.fernandez@mediasmart.es>
2015-01-04 22:46:18 +0100

    Export remote and pooled clients.

--- index.js
+++ index.js
@@ -8,7 +8,13 @@
 
 // requires
 var server = require('./lib/server.js');
+var remote = require('./lib/remote.js');
+var pooled = require('./lib/pooled.js');
 
 // exports
 exports.start = server.start;
+exports.pooledConnect = pooled.pooledConnect;
+exports.remoteConnect = remote.remoteConnect;
+exports.PooledClient = pooled.PooledClient;
+exports.RemoteClient = remote.RemoteClient;
 
